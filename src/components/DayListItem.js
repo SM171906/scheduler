2f936901e06ec398fb50f9f87e3166c07a60703c
Smiti <mishra.smiti06@gmail.com>
2021-10-26 21:14:26 -0400

    test the Daylist Items

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,33 @@
 import classNames from "classnames";
 import React from "react";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props){
-  
+  const { name, spots, selected, setDay } = props;
+  const dayClass = classNames ("day-list__item", {
+    
+    "day-list__item--selected": selected,
+    "day-list__item--full":!spots
+   
+  })
+  function formatSpots (){
+    if (!spots) {
+      return "no spots remaining";
+    }
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+    return `${spots} spots remaining `;
+  }
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular"> {props.name} </h2>
-      <h3 className="text--light"> {props.spots} spots remaining </h3>
-    </li>
+      <li onClick={() => setDay(name)} className={dayClass}>
+        <h2 className="text--regular">{props.name}</h2>
+        <h3 className="text--light">{formatSpots()}</h3>
+      </li>
+    
   );
 }
+
+
+
+
--- stories/index.js
+++ stories/index.js
@@ -27,9 +27,9 @@ storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   }) 
-  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
-  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
-  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Unselected", () => <DayListItem setDay={action("setDay")} name="Monday" spots={5} />) 
+  .add("Selected", () => <DayListItem setDay={action("setDay")} name="Monday" spots={5} selected={true} />) 
+  .add("Full", () => <DayListItem setDay={action("setDay")} name="Monday" spots={0} />)
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} 
   />));
