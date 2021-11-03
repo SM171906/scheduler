7091a63ca96db72939996a17fe6f09b5ed4bd266
Smiti <mishra.smiti06@gmail.com>
2021-11-03 16:02:33 -0400

    update spots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 export default function useApplicationData (){
+
   const [state, setState] = useState({
     day: "",
     days: [],
@@ -9,7 +10,9 @@ export default function useApplicationData (){
   });
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState({ ...state, days });
+  
   function bookInterview(id, interview) {
+    
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -18,12 +21,16 @@ export default function useApplicationData (){
       ...state.appointments,
       [id]: appointment
     };
+    const days = spotsLeftForDay(state, appointments);
     return axios.put(`/api/appointments/${id}`, { interview })
       .then(() => {
         setState({ ...state, appointments })
+        setState((prev) => ({ ...prev, days, appointments }));
       })
   }
+
   function cancelInterview(id) {
+    
     const appointment = {
       ...state.appointments[id],
       interview: null,
@@ -32,10 +39,31 @@ export default function useApplicationData (){
       ...state.appointments,
       [id]: appointment
     };
+    const days = spotsLeftForDay(state, appointments);
     return axios.delete(`/api/appointments/${id}`)
       .then(() => {
         setState({ ...state, appointments })
+        setState((prev) => ({ ...prev, days, appointments }));
+
       })
+      
+      
+  }
+
+  function spotsLeftForDay(state, appointments) {
+    let count = 0;
+    const filteredDay = state.days.find((d) => d.name === state.day);
+    for (const elem of filteredDay.appointments) {
+      if (appointments[elem].interview === null) count++;
+    }
+    const updatedDay = {
+      ...filteredDay,
+      spots: count,
+    };
+    const days = state.days.map((day) =>
+      day.id === filteredDay.id ? updatedDay : day
+    );
+    return days;
   }
 
   useEffect(() => {
