24e19865013ef4a9058d75d722107b18b13c408e
Smiti <mishra.smiti06@gmail.com>
2021-11-03 20:45:57 -0400

    update cypress test

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -16,6 +16,7 @@ export default function InterviewerListItem(props) {
      <img className="interviewers__item-image" 
      src={props.avatar}
      alt={props.name}
+     
      />
      {props.selected && props.name}
    </li>
